ad592b975d5bb1729b3d8a475bf43276520d1c68
hoseinmoradi <moradih7379@gmail.com>
2023-08-15 09:48:05 +0330

    improve security in firebase.

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-kzQ8P9-USP3Y4PHV7-SbOsQ5X_ov9Ss",
-  authDomain: "tarkhineh-9ade7.firebaseapp.com",
-  projectId: "tarkhineh-9ade7",
-  storageBucket: "tarkhineh-9ade7.appspot.com",
-  messagingSenderId: "30538674122",
-  appId: "1:30538674122:web:5463748fce81ab25122933",
-  measurementId: "G-8D9G26NSC5",
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
